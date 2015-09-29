fd66acd5015c80c621ca861e530164c41da3f356
Kristian Hildebrandt <kristian@Kristians-MacBook-Pro.local>
2015-09-29 19:39:42 +0200

    building non-active-record-model wich will be used for form

---
+++ app/models/offer_wall.rb
@@ -0,0 +1,8 @@
+class OfferWall
+  include ActiveModel::Model
+
+  attr_accessor :uid, :pub0, :page
+
+  validates_presence_of :uid
+  validates_numericality_of :page, only_integer: true
+end
---
+++ spec/models/offer_wall_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+describe OfferWall do
+  %i(uid pub0 page).each do |attr|
+    it { is_expected.to respond_to(attr) }
+  end
+
+  it { is_expected.to validate_presence_of(:uid) }
+  it { is_expected.to validate_numericality_of(:page).only_integer }
+end
